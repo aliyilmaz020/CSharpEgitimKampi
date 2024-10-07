5817a67e4382df466a45ff3fbf8e8b610eca00dd
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2024-10-08 00:26:00 +0300

    if else

--- 03_MakingDecision/Program.cs
+++ 03_MakingDecision/Program.cs
@@ -10,6 +10,112 @@ namespace _03_MakingDecision
     {
         static void Main(string[] args)
         {
+            #region If Else
+
+            //Console.Write("Lütfen şifreyi giriniz: ");
+            //string password;
+            //password = Console.ReadLine();
+
+            //if (password == "abcd")
+            //{
+            //    Console.WriteLine("Şifre Doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şifre Yanlış");
+            //}
+
+            //string capital, country;
+            //Console.Write("Başkenti Giriniz: ");
+            //capital = Console.ReadLine();
+
+            //Console.Write("Ülkeyi Giriniz: ");
+            //country = Console.ReadLine();
+
+            //if (capital == "ankara" &country == "türkiye")
+            //{
+            //    Console.WriteLine("Veriler Doğrulandı.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hatalı Bilgi");
+            //}
+
+            //int number;
+            //Console.Write("Sayı giriniz: ");
+            //number = int.Parse(Console.ReadLine());
+
+            //if (number == 5)
+            //{
+            //    Console.WriteLine("Sayı doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayı hatalı");
+            //}
+
+            //int exam1, exam2, exam3, average;
+            //string result = "Hata";
+
+            //Console.Write("Sınav1: ");
+            //exam1 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Sınav2: ");
+            //exam2 = int.Parse(Console.ReadLine());
+
+            //Console.Write("Sınav3: ");
+            //exam3 = int.Parse(Console.ReadLine());
+
+            //average = (exam1+exam2 + exam3) / 3;
+            //Console.WriteLine("Sınavların Ortalaması: " + average);
+
+            //if (average > 0 & average <= 50)
+            //{
+            //    result = "Sonuç Vasat";
+            //}
+            //if (average > 50 & average <= 70)
+            //{
+            //    result = "Sonuç Orta";
+            //}
+            //if(average > 70 & average <= 84)
+            //{
+            //    result = " Sonuç İyi";
+            //}
+            //if(average > 85 & average <= 100)
+            //{
+            //    result = "Sonuç Çok İyi";
+            //}
+
+            //Console.WriteLine(result);
+
+            //string city;
+            //Console.Write("Lütfen şehir girişi yapınız: ");
+            //city = Console.ReadLine();
+
+            //if (city == "adana" | city == "ankara" | city == "bursa" | city == "trabzon")
+            //{
+            //    Console.WriteLine("şehir mevcut");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("şehir mevcut değil");
+            //}
+
+            //Console.Write("Lütfen Kullanıcı Adını Giriniz: ");
+            //string username = Console.ReadLine();
+
+            //if (username != "admin")
+            //{
+            //    Console.WriteLine("Bu kullanıcı adı kabul edilemez");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Hoş Geldiniz");
+            //}
+            #endregion
+
+            //Console.Read();
+
         }
     }
 }
