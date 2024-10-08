620b5ce510a43fed87adf887e2025ac9818c800b
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2024-10-08 19:51:22 +0300

    for loop

--- 04_Loops/Program.cs
+++ 04_Loops/Program.cs
@@ -10,6 +10,35 @@ namespace _04_Loops
     {
         static void Main(string[] args)
         {
+            #region For Döngüsü
+
+            //int i;
+
+            //for (i = 1; i <= 5; i++)
+            //{
+            //    Console.WriteLine("C# Eğitim Kampı");
+            //}
+
+            //for (int i = 1; i <= 20; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //for (int i = 3; i <= 50; i += 3)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            //Console.Write("Lütfen ekrana yazılmasını istediğiniz adedi giriniz: ");
+            //int finishValue = int.Parse(Console.ReadLine());
+
+            //for (int i = 1; i <= finishValue; i++) 
+            //{ 
+            //    Console.WriteLine("Yaşasın Cumhuriyet");
+            //}
+            #endregion
+
+            Console.Read();
         }
     }
 }
