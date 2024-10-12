52b50f003ec052183fe7844ae5ff83f159c67038
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2024-10-12 15:42:42 +0300

    Dizideki Tüm Elemanları Listeleme

--- 06_Arrays/Program.cs
+++ 06_Arrays/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _06_Arrays
 {
@@ -44,7 +40,61 @@ namespace _06_Arrays
             //Console.WriteLine(cities[2]);
             #endregion
 
+            #region Dizideki Tüm Elemanları Listeleme
+            //string[] colors = { "Sarı", "Kırmızı", "Beyaz", "Mavi", "Yeşil", "Turuncu", "Pembe" };
 
+            //for (int i = 0; i < colors.Length; i++) 
+            //{
+            //    Console.WriteLine(colors[i]);
+            //}
+
+            //int[] number = { 4, 85, 96, 75,125, 635, 488, 522,7456, 2365, 1120 };
+
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    if (number[i] % 3 == 0) 
+            //    {
+            //        Console.WriteLine(number[i]);
+            //    }
+            //}
+
+            //char[] symbols = { 'a', 'b', 'c', '*', '/', '-' };
+            //for (int i = 0; i < symbols.Length; i++)
+            // {
+            //     Console.WriteLine(symbols[i]);
+            // }
+
+            //int[] myArray = { 47, 85, 95, 41, 25, 635, 789, 86, 100 };
+
+            //int maxNumber = myArray[0];
+
+            //for(int i = 0;i < myArray.Length;i++)
+            //{
+            //    if(myArray[i] > maxNumber)
+            //    {
+            //        maxNumber = myArray[i];
+            //    }
+            //}
+
+            //Console.WriteLine(maxNumber);
+
+            //string[] persons = { "ali", "ahmet", "ayşe", "buse", "cem", "deniz" };
+            //Console.WriteLine(persons.Length);
+
+            //int[] number= {5, 95, 41, 25, 635, 789, 86, 100 };
+            //Array.Sort(number);
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    Console.WriteLine(number[i]);
+            //}
+
+            //int[] numbers = { 5, 95, 41, 25, 635, 789, 86, 100 };
+            //Array.Reverse(numbers);
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    Console.WriteLine(numbers[i]);
+            //}
+            #endregion
             Console.Read();
         }
     }
