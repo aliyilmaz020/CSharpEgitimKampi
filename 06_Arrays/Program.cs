cfe1e8b006280f644a030b14153c6ebf1f3e4569
aliyilmaz020 <ali.yilmaz.199920@icloud.com>
2024-10-12 14:38:22 +0300

    Temel dizi örnekleri

--- 06_Arrays/Program.cs
+++ 06_Arrays/Program.cs
@@ -10,6 +10,19 @@ namespace _06_Arrays
     {
         static void Main(string[] args)
         {
+            #region Temel Dizi Örnekleri
+
+            //string[] colors = new string[4];
+            //colors[0] = "Kırmızı";
+            //colors[1] = "Sarı";
+            //colors[2] = "Beyaz";
+            //colors[3] = "Mavi";
+
+            //Console.WriteLine(colors[2]);
+
+            #endregion
+
+            Console.Read();
         }
     }
 }
